c628d82a91c778d89b67cccaa7ba25e9f26dec7e
Leiverin <93923726+Leiverin@users.noreply.github.com>
2021-11-20 14:52:29 +0700

    Update BrandAdapter.java

--- MalthShop2/app/src/main/java/AdapterHome/BrandAdapter.java
+++ MalthShop2/app/src/main/java/AdapterHome/BrandAdapter.java
@@ -4,7 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.ImageView;sadád
+
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
